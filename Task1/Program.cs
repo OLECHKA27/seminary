690501da1512177d22d72fd76768ee0252a90b0c
RUTT <rutt@inbox.ru>
2025-02-19 00:28:00 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число A:");
+int A =int.Parse(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B =int.Parse(Console.ReadLine());
+
+//if (A == B*B)
+//if (B==A/B)
+if (Math.Pow(B, 2)==A)
+{Console.Write("Да");}
+else
+{Console.Write("Нет");}
