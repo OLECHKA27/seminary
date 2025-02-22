068b87ff6092122bc2e1e7352ba0e99d9a90e6ca
RUTT <rutt@inbox.ru>
2025-02-22 15:28:51 +0300

    ДЗ 1Определите, делится ли число на другое

---
+++ TaskDZ/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine());
+if (M==0)
+{
+  Console.WriteLine("Ошибка! Делить на ноль нельзя.");
+}
+
+else if (N%M == 0)
+{
+Console.Write($"{N} делится на {M}");
+}
+else 
+{
+ Console.Write($"{N} не делится на {M}");   
+}
