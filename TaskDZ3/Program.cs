4373ea80b0743093ef9e101ff835d71552ae956f
RUTT <rutt@inbox.ru>
2025-02-23 23:04:51 +0300

    ДЗ3 Положительные и отрицательные

---
+++ TaskDZ3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число N: ");
+int N= int.Parse(Console.ReadLine());
+if (N>0)
+{
+    Console.Write($"Число N= {N} Положительное ");
+}
+else if (N<0)
+{
+    Console.Write($"Число N= {N} Отрицательное ");
+}
+else
+Console.Write($"Число N : *{N}* ");
