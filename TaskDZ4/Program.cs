789f40edad4cfac72721b4806f9d773b9d842a8a
RUTT <rutt@inbox.ru>
2025-02-23 23:30:26 +0300

    ДЗ4 нечетные числа

---
+++ TaskDZ4/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите число N: ");
+        int N = int.Parse(Console.ReadLine());
+        int A = 1;
+        if (N < 1)
+        {
+            Console.Write("N Должно быть более или равно 1");
+        }
+        else
+            while (A <= N)
+            {
+                Console.Write($" {A} ");
+                A = A + 2;
+            }
+    }
+}
